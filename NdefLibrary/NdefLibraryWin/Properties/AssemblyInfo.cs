055cc950ea6c132e3b2a2dc1684c15df70b61a03
Andreas Jakl <andreas.jakl@live.com>
2015-11-05 15:15:01 -0800

    Update Windows 10 UI.

--- NdefLibrary/NdefLibraryWin/Properties/AssemblyInfo.cs
+++ NdefLibrary/NdefLibraryWin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NDEF Library for Proximity APIs / NFC (Windows 8.1 Extension)")]
+[assembly: AssemblyTitle("NDEF Library for Proximity APIs / NFC (Windows Extension)")]
 [assembly: AssemblyDescription("Easily parse and create NDEF records with the Windows Proximity APIs / Near Field Communication NFC.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andreas Jakl")]
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
 [assembly: ComVisible(false)]
