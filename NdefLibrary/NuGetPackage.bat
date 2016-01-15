bdba5d7f80b0521166cc92b6aa8a0ba52fbe54bd
Andreas Jakl <andreas.jakl@live.com>
2016-01-15 16:43:47 +0100

    Remove NuGet Reference Generator again - no longer needed, the nuspec is updated. Prepare for 3.1.0 release.

--- NdefLibrary/NuGetPackage.bat
+++ NdefLibrary/NuGetPackage.bat
@@ -2,7 +2,9 @@
 @echo Remember to update the release notes in /NdefLibrary/NdefLibrary.nuspec
 @echo --------------------------------------------------------------------------
 nuget pack ./NdefLibrary.nuspec -OutputDirectory ./nupkg -Build -Symbols -Prop Configuration=Release
-nuget pack ./NdefLibraryExtension.nuspec -OutputDirectory ./nupkg -Build -Symbols -Prop Configuration=Release
+@rem nuget pack ./NdefLibraryExtension.nuspec -OutputDirectory ./nupkg -Build -Symbols -Prop Configuration=Release
+
+
 @rem nuget pack ./NdefLibrary/NdefLibrary/NdefLibrary.csproj -Symbols -Prop Configuration=Release
 @rem http://docs.nuget.org/docs/creating-packages/creating-and-publishing-a-package
 
