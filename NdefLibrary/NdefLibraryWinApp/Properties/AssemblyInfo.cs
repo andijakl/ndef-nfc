89d2e8068cc69ed094c93f0c21fdc24858a5844b
Andreas Jakl <andreas.jakl@live.com>
2025-09-12 16:38:44 +0200

    First step to update the UWP version of the library to the Windows App SDK (WinUI3)

---
+++ NdefLibrary/NdefLibraryWinApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NDEF Library Windows App SDK Extensions")]
+[assembly: AssemblyDescription("Windows App SDK (WinUI 3) extension helpers for NDEF library")]
+[assembly: AssemblyCompany("Andreas Jakl")]
+[assembly: AssemblyProduct("NdefLibraryWinApp")]
+[assembly: AssemblyCopyright("Copyright © 2012-2025 Andreas Jakl")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
+
